37d85aa057584e0be27c365b862fec6826648586
shakira <user@codeox-3.bbrouter>
2022-12-23 12:37:33 +0530

    developed tree,form,kanban view, search view, group by, filters

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Hospital management',
+    'version': '15.0.0.1',
+    'summary': 'hospital management',
+    'sequence': -100,
+    'author':'code-ox',
+    'description': """hospital management""",
+    'website': 'https://code-ox.com',
+    'category': 'Extra tools',
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3'
+}
---
+++ models/patient.py
@@ -0,0 +1,14 @@
+from  odoo import api, fields, models
+
+class hospiatlpatient(models.Model):
+    _name = 'hospital.patient'
+    _description = "hospial patient"
+
+    name = fields.Char(string ='Name')
+    age = fields.Integer(string = 'Age')
+    gender=fields.Selection([('male','Male'),('female','Female')], string="Gender")
+    address= fields.Text(string='Address')
+    bloodgroup=fields.Char(string='Blood group')
+
+
+    
