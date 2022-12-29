f118fe50b7734ca46b24090f53eb893980e6b9b3
shakira <user@codeox-3.bbrouter>
2022-12-29 16:30:24 +0530

    Added created a compute field for age using date of birth, onchange function is used for reference field, added notebook and pages

--- models/appointment.py
+++ models/appointment.py
@@ -9,6 +9,12 @@ class appointmentpatient(models.Model):
     appointment_date = fields.Datetime(string="Appointment date", default=fields.Datetime.now)
     booking_date = fields.Date(string="Booking date", default = fields.Date.context_today)
     gender=fields.Selection([('male','Male'),('female','Female')], string="Gender", related ='patient_id.gender')
-   
+    ref = fields.Char(string = 'Reference')
 
-    
+
+
+
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
--- models/patient.py
+++ models/patient.py
@@ -1,3 +1,4 @@
+from datetime import date
 from  odoo import api, fields, models
 
 class hospitalpatient(models.Model):
@@ -6,11 +7,25 @@ class hospitalpatient(models.Model):
     _description = "hospital patient"
 
     name = fields.Char(string ='Name', tracking = True)
-    age = fields.Integer(string = 'Age')
+    date_of_birth =fields.Date(string='Date of Birth')
+    age = fields.Integer(string = 'Age', compute = '_compute_age')
     gender=fields.Selection([('male','Male'),('female','Female')], string="Gender")
     address= fields.Text(string='Address')
     bloodgroup=fields.Char(string='Blood group')
     active=fields.Boolean(string="Active", default = True)
+    ref = fields.Char(string = 'Reference')
+ 
+
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for rec in self:
+            today = date.today() 
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 0
+
+
 
 
     
